4de95fb09dc4b14f419d79c5b951d91ecbec945e
nReyna <nabor.reyna@gmail.com>
2018-07-31 10:52:15 -0700

    hierarchy change, yelp schema, readme update

--- queries.sql
+++ queries.sql
@@ -8,3 +8,16 @@ SELECT r.id, r.stars, b.name, c.category FROM review r
 INNER JOIN business b ON r.business_id = b.id 
 INNER JOIN category c ON c.business_id = b.id
 LIMIT 3;
+
+SELECT r.id, r.stars, b.name, c.category FROM review r
+INNER JOIN business b ON r.business_id = b.id 
+INNER JOIN category c ON c.business_id = b.id
+WHERE c.category in ("Chinese", "Mexican")
+LIMIT 3;
+
+SELECT r.id, r.stars, r.date, r.text, r.useful, r.funny, r.cool, b.name, b.state, c.category FROM review r
+INNER JOIN business b ON r.business_id = b.id 
+INNER JOIN category c ON c.business_id = b.id
+WHERE c.category in ("Chinese", "Mexican")
+LIMIT 3;
+
