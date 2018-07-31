f3c2b09e67b10f76687a56b8724ad7a59a24598a
nReyna <nabor.reyna@gmail.com>
2018-07-31 13:19:37 -0700

    Query update, table creation, and prep for jupyter/pandas interaction

--- queries.sql
+++ queries.sql
@@ -15,9 +15,9 @@ INNER JOIN category c ON c.business_id = b.id
 WHERE c.category in ("Chinese", "Mexican")
 LIMIT 3;
 
-SELECT r.id, r.stars, r.date, r.text, r.useful, r.funny, r.cool, b.name, b.state, c.category FROM review r
+SELECT r.id, r.stars, r.date, r.text, r.useful, r.funny, r.cool, b.name, b.city, b.state, c.category FROM review r
 INNER JOIN business b ON r.business_id = b.id 
 INNER JOIN category c ON c.business_id = b.id
-WHERE c.category in ("Chinese", "Mexican")
-LIMIT 3;
+WHERE c.category in ("Mexican", "Chinese", "Indian", "Vegetarian", "Vegan") AND b.state in ("OH")
+LIMIT 30;
 
